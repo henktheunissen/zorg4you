ef1447fd5ce70ac0c2cfdd1f9a0fd57943c515fc
Henk <theunissen.henk@gmail.com>
2018-10-03 13:29:36 +0200

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -1,12 +1,13 @@
 <div class="header">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <img src="../img/logo.jpg" class="header_image">
 </div>
 <div class="content">
     <div class="header_nav">
         <ul class="nav nav-pills">
-            <li role="presentation"><a href="index.php">Werknemers</a></li>
-            <li role="presentation"><a href="klanten.php">Klanten</a></li>
-            <li role="presentation"><a href="diensten.php">Werksoorten</a></li>
+            <li role="presentation"><a href="index.php"><i class="fas fa-user"></i> Werknemers</a></li>
+            <li role="presentation"><a href="klanten.php"><i class="fas fa-user"></i> Klanten</a></li>
+            <li role="presentation"><a href="diensten.php"><i class="fas fa-briefcase"></i> Werksoorten</a></li>
         </ul>
     </div>
 
